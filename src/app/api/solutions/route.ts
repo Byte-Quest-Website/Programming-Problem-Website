b3eca47b09e0f0878eb2cbc91c3b5824c4318f58
FusionSid <siddheshadsv@icloud.com>
2023-08-20 23:23:42 +1200

    feat: add route to get solution(s)

---
+++ src/app/api/solutions/route.ts
@@ -0,0 +1,60 @@
+import { z } from "zod";
+import { getServerSession } from "next-auth";
+import { NextResponse, NextRequest } from "next/server";
+
+import { prisma } from "@/core/prisma/connections";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function GET(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return NextResponse.json(
+            {
+                success: false,
+                detail: "Not Authenticated",
+            },
+            { status: 401 }
+        );
+    }
+    const parsedURL = new URL(request.url);
+
+    let id = parsedURL.searchParams.get("id");
+    if (id && !z.string().uuid().safeParse(id).success) {
+        return NextResponse.json(
+            {
+                success: false,
+                detail: "Invalid id provided. Must be a UUID",
+            },
+            { status: 400 }
+        );
+    }
+
+    if (!id) {
+        const solutions = await prisma.solution.findMany();
+        return NextResponse.json({
+            success: true,
+            solutions: solutions,
+        });
+    }
+
+    if (id) {
+        const solution = await prisma.solution.findUnique({
+            where: { id: id },
+        });
+        if (solution) {
+            return NextResponse.json({
+                success: true,
+                solution: solution,
+            });
+        }
+    }
+
+    return NextResponse.json(
+        {
+            success: false,
+            detail: "solution not found",
+        },
+        { status: 404 }
+    );
+}
