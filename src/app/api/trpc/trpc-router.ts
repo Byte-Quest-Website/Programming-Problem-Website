626b4b30aec9e81850137994ed0ea361ca21fa7a
FusionSid <siddheshadsv@icloud.com>
2023-08-22 09:27:27 +1200

    chore: remove test routes

--- src/app/api/trpc/trpc-router.ts
+++ src/app/api/trpc/trpc-router.ts
@@ -1,76 +1,18 @@
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
+import { prisma, redis } from "@/core/prisma/connections";
 
 const t = initTRPC.create({
     transformer: superjson,
 });
 
 export const appRouter = t.router({
-    getUsers: t.procedure.query(async ({ ctx }) => {
-        await new Promise(resolve => setTimeout(resolve, 1000))
-        return userList;
+    getAllSolutions: t.procedure.query(async ({ ctx }) => {
+        return prisma.solution.findMany();
+    }),
+    getAllProblems: t.procedure.query(async ({ ctx }) => {
+        return prisma.solution.findMany();
     }),
 });
 
 export type AppRouter = typeof appRouter;
-
-// this is just placeholder for now:
-
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-}
-
-const userList: User[] = [
-    {
-        id: "1",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-    },
-    {
-        id: "2",
-        name: "Abraham Smith",
-        email: "abrahamsmith@gmail.com",
-    },
-    {
-        id: "3",
-        name: "Barbie Tracy",
-        email: "barbietracy@gmail.com",
-    },
-    {
-        id: "4",
-        name: "John Payday",
-        email: "johnpayday@gmail.com",
-    },
-    {
-        id: "5",
-        name: "Remember My Name",
-        email: "remembermyname@gmail.com",
-    },
-    {
-        id: "6",
-        name: "Go to School",
-        email: "gotoschool@gmail.com",
-    },
-    {
-        id: "7",
-        name: "Fish Fruit",
-        email: "fishfruit@gmail.com",
-    },
-    {
-        id: "8",
-        name: "Don't try",
-        email: "donttry@gmail.com",
-    },
-    {
-        id: "9",
-        name: "Producer Feed",
-        email: "producerfeed@gmail.com",
-    },
-    {
-        id: "10",
-        name: "Panic So",
-        email: "panicso@gmail.com",
-    },
-];
