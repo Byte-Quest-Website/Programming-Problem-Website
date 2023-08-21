c3ee648ef53913470c2a57740c533cba7a4ace79
FusionSid <siddheshadsv@icloud.com>
2023-08-22 09:27:48 +1200

    feat: add sign in and signout buttons to navbar

--- src/core/components/navbar.tsx
+++ src/core/components/navbar.tsx
@@ -1,7 +1,41 @@
+"use client";
+
 import React from "react";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const { data: session } = useSession();
+    return (
+        <div>
+            <ul>
+                <li>
+                    {session ? (
+                        <>
+                            <button
+                                onClick={() => {
+                                    signOut();
+                                }}
+                            >
+                                Sign Out
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <button
+                                onClick={() => {
+                                    signIn();
+                                }}
+                            >
+                                Sign In
+                            </button>
+                        </>
+                    )}
+                </li>
+                <li></li>
+            </ul>
+        </div>
+    );
 };
 
 export default Navbar;
