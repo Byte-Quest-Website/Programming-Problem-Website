24184d25f4d7fb4f9c912e4f5448ae988c74a1a9
FusionSid <siddheshadsv@icloud.com>
2023-08-22 09:28:00 +1200

    feat: create problem item component for problems page

---
+++ src/core/components/problemItem.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Problem } from "@prisma/client";
+
+const ProblemItem = ({ problem }: { problem: Problem }) => {
+    const router = useRouter();
+
+    return (
+        <div onClick={() => router.push(`http://localhost:3000/problems/${problem.id}`)}>
+            <li>{problem.id}</li>
+            <li>{problem.title}</li>
+            <li>{problem.description}</li>
+            <li>{problem.functionName}</li>
+            <li>{problem.createdAt.toDateString()}</li>
+            <li>{problem.updatedAt.toDateString()}</li>
+            <li>{problem.verified}</li>
+        </div>
+    );
+};
+
+export default ProblemItem;
