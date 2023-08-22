4a2d48e99f8dbb906525835fa50db75eb037b116
FusionSid <siddheshadsv@icloud.com>
2023-08-22 23:47:43 +1200

    feat: add custom fonts support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -11,6 +9,25 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        one: "#2B2D42",
+        two: "#8D99AE",
+        three: "#EDF2F4",
+        four: "#EF233C",
+        five: "#D80032",
+        six: "#0080DC",
+        seven: "#181921",
+        eight: "#EDF2F4",
+        nine: "#FBBEBE",
+      },
+      fontFamily: {
+        jetbrains: ["JetbrainsMono"],
+        bebas: "Bebas Neue",
+        poppins: "Poppins",
+        poppinsbold: "PoppinsBold",
+        roboto: "Roboto",
+        hacked: ["Hacked"],
       },
     },
   },
