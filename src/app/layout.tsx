0b40d1d9c8e423d43517d5ed3761a9c642a78df4
FusionSid <siddheshadsv@icloud.com>
2023-09-04 12:02:29 +1200

    fix: disable digusting scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
 
     return (
         <html lang="en">
-            <body className="bg-[#181921]">
+            <body className="bg-[#181921] no-scrollbar">
                 <Provider session={session}>
                     <TrpcProvider>
                         <Navbar />
