e1e85caec938507a3d73ddb306907b7a87273c15
FusionSid <siddheshadsv@icloud.com>
2023-09-05 10:14:52 +1200

    feat: auto save editor state to localstorage so users don't loose code

--- src/core/components/ProblemEditor.tsx
+++ src/core/components/ProblemEditor.tsx
@@ -1,43 +1,100 @@
 "use client";
 
 import Markdown from "./markdown";
-import React, { useState, useEffect, useRef } from "react";
+import THEMES from "../helpers/themes";
 import { Problem, User } from "@prisma/client";
 import CodeMirror from "@uiw/react-codemirror";
 import { python } from "@codemirror/lang-python";
 import "split-pane-react/esm/themes/default.css";
 import SplitPane, { Pane } from "split-pane-react";
-import THEMES from "../helpers/themes";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 
 const extensions = [python()];
 
 const ProblemEditor = (props: { problem: Problem; author: User }) => {
-    const [fullScreen, setFullScreen] = useState(false);
     const [currentOutput, setCurrentOutput] = useState("");
+
     const [code, setCode] = useState("");
-    const [completion, setCompletion] = useState(true);
+    const [autosave, setAutosave] = useState(true);
+    const [codingTime, setCodingTime] = useState(false);
     const [theme, setTheme] = useState(THEMES.atomone);
+
+    const [fullScreen, setFullScreen] = useState(false);
     const [sizes, setSizes] = useState([250, "30%", "auto"]);
+
+    const [completion, setCompletion] = useState(true);
     const [syntaxHighlighting, setSyntaxHighlighting] = useState(true);
 
     useEffect(() => {
+        // if full screen is selected resize breif div to take 0 width
         if (fullScreen) {
             return setSizes((sizes) => [0, sizes[1], sizes[2]]);
         }
         setSizes((sizes) => [250, sizes[1], sizes[2]]);
     }, [fullScreen]);
 
-    const ref = useRef<HTMLDivElement>(null);
-
-    const scrollToElement = () => {
-        ref.current?.scrollIntoView({ behavior: "smooth" });
-    };
-
+    const editorDivRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
-        scrollToElement();
+        editorDivRef.current?.scrollIntoView({ behavior: "smooth" });
     }, []);
 
-    async function makeRequest() {
+    useEffect(() => {
+        const themeName = Object.keys(THEMES).find((key) => {
+            return THEMES[key] === theme;
+        });
+        // if light mode is selected remove auto complete and syntax highlighting
+        if (themeName?.toLowerCase().includes("light")) {
+            alert(
+                "You sir have commited a war crime by using light mode\n\
+                There goes your syntax highlighting and autocomplete"
+            );
+            setCompletion(false);
+            setSyntaxHighlighting(false);
+            return;
+        }
+        setCompletion(true);
+        setSyntaxHighlighting(true);
+    }, [theme]);
+
+    const onChange = useCallback((value: string, viewUpdate) => {
+        // update code state when code is changed
+        setCode(value);
+    }, []);
+
+    useEffect(() => {
+        if (!completion || !syntaxHighlighting) {
+            return;
+        }
+
+        // if global variable or wildcard import is written remove auto complete and syntax highlighting
+        if (code.includes("global")) {
+            alert(
+                "Bro just typed 'global'\n\
+                No more autocomplete and syntaxhighlighting for you"
+            );
+            setCompletion(false);
+            setSyntaxHighlighting(false);
+        } else if (
+            code.includes(
+                "import *"
+            ) /* special case for mr sheppard's tkinter code */
+        ) {
+            alert(
+                "C'mon Mr Sheppard! Wildcard imports are NOT allowed here.\n\
+                Uh Oh there goes your autocompletion and syntax highlighting"
+            );
+            setCompletion(false);
+            setSyntaxHighlighting(false);
+        }
+
+        if (codingTime && autosave) {
+            localStorage.setItem(`${props.problem.id}-editor-code`, code);
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [code]);
+
+    const makeRequest = useCallback(async (id: string, code: string) => {
         let response;
         try {
             response = await fetch("https://rce.fusionsid.xyz/testcode", {
@@ -48,7 +105,7 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
                 },
                 body: JSON.stringify({
                     code: code,
-                    problem_id: props.problem.id,
+                    problem_id: id,
                 }),
                 cache: "no-store",
             });
@@ -102,7 +159,7 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
                 }
             }, 1000);
         }
-    }
+    }, []);
 
     const difficultyColors = {
         EASY: "#A5D76E",
@@ -110,56 +167,36 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
         HARD: "#D15559",
     };
 
-    useEffect(() => {
-        const name = Object.keys(THEMES).find((key) => {
-            return THEMES[key] === theme;
-        });
-        if (name?.toLowerCase().includes("light")) {
-            alert(
-                "You sir have commited a war crime by using light mode\nThere goes your syntax highlighting and autocomplete"
-            );
-            setCompletion(false);
-            setSyntaxHighlighting(false);
-            return;
-        }
-        setCompletion(true);
-        setSyntaxHighlighting(true);
-    }, [theme]);
-
-    const onChange = React.useCallback(
-        (value: string, viewUpdate) => {
-            setCode(value);
-
-            if (!completion || !syntaxHighlighting) {
-                return;
-            }
-            if (value.includes("global")) {
-                alert(
-                    "Bro just typed 'global'\nNo more autocomplete and syntaxhighlighting for you"
-                );
-                setCompletion(false);
-                setSyntaxHighlighting(false);
-            } else if (
-                value.includes(
-                    "import *"
-                ) /* special case for mr sheppard's code */
-            ) {
-                alert(
-                    "C'mon Mr Sheppard! Wildcard imports are NOT allowed here.\nUh Oh there goes your autocompletion and syntax highlighting"
-                );
-                setCompletion(false);
-                setSyntaxHighlighting(false);
-            }
-        },
-        [completion, syntaxHighlighting]
-    );
-
-    const defaultCode = `def ${
+    const defaultBoilerplateCode = `def ${
         props.problem.functionName
     }(${props.problem.parameterNames.join(", ")}):\n  pass`;
 
+    useEffect(() => {
+        const noAutoSave =
+            localStorage.getItem(`${props.problem.id}-editor-autosave`) ===
+            "false";
+        if (noAutoSave) {
+            setCode(defaultBoilerplateCode);
+            return setAutosave(false);
+        }
+
+        const previousCode = localStorage.getItem(
+            `${props.problem.id}-editor-code`
+        );
+        if (previousCode) {
+            setCode(previousCode);
+        } else {
+            setCode(defaultBoilerplateCode);
+        }
+        setCodingTime(true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
-        <div ref={ref} className="h-screen snap snap-y snap-mandatory m-1">
+        <div
+            ref={editorDivRef}
+            className="h-screen snap snap-y snap-mandatory m-1"
+        >
             {/* @ts-ignore */}
             <SplitPane
                 className="snap-start"
@@ -248,6 +285,31 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
                             Code Editor
                         </h1>
                         <div className="flex items-center gap-4">
+                            <div>
+                                <div className="flex items-center p-2 rounded">
+                                    <input
+                                        type="checkbox"
+                                        checked={autosave}
+                                        onChange={(e) => {
+                                            const newSaveValue = !autosave;
+                                            localStorage.setItem(
+                                                `${props.problem.id}-editor-autosave`,
+                                                newSaveValue ? "true" : "false"
+                                            );
+                                            setAutosave(newSaveValue);
+                                            if (newSaveValue) {
+                                                localStorage.removeItem(
+                                                    `${props.problem.id}-editor-code`
+                                                );
+                                            }
+                                        }}
+                                        className="w-4 h-4 bg-gray-600 border-gray-500"
+                                    />
+                                    <label className="ml-2 text-md font-poppins rounded text-white">
+                                        Save
+                                    </label>
+                                </div>
+                            </div>
                             <div className="flex gap-2 items-center">
                                 <h1 className="text-white font-poppins">
                                     Theme:
@@ -276,7 +338,7 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
                                 >
                                     <path
                                         d="M1158.513-.012v123.68h550.5L123.68 1708.878V1158.5H0V1920h761.5v-123.68H211.121l1585.21-1585.21v550.5h123.68V-.011z"
-                                        fill-rule="evenodd"
+                                        fillRule="evenodd"
                                     />
                                 </svg>
                             </button>
@@ -289,7 +351,7 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
                         <CodeMirror
                             className="rounded-md p-2 h-full"
                             theme={theme[0]}
-                            value={defaultCode}
+                            value={code}
                             autoFocus={true}
                             extensions={extensions}
                             indentWithTab={true}
@@ -302,8 +364,9 @@ const ProblemEditor = (props: { problem: Problem; author: User }) => {
                     </div>
                     <div className="bg-one h-full rounded-md shadow-xl p-7 overflow-scroll no-scrollbar">
                         <button
+                            className="text-white"
                             onClick={async () => {
-                                await makeRequest();
+                                await makeRequest(code, props.problem.id);
                             }}
                         >
                             Run
