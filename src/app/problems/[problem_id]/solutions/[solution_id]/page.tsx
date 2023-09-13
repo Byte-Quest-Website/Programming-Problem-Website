fcda4dd6bf64a4e48ab8ea8b4c055f7931cee201
FusionSid <siddheshadsv@icloud.com>
2023-09-13 23:59:47 +1200

    feat: must solve the problem to see others solutions

--- src/app/problems/[problem_id]/solutions/[solution_id]/page.tsx
+++ src/app/problems/[problem_id]/solutions/[solution_id]/page.tsx
@@ -13,6 +13,28 @@ const page = async ({
     const session = await getServerSession(authOptions);
     if (!session) return <>NOT LOGGED IN</>;
 
+    const user = await prisma.user.findUnique({
+        where: { id: session.user.id },
+    });
+    if (!user) {
+        return <>Failed :(</>;
+    }
+
+    const solved =
+        (
+            await prisma.solution.findMany({
+                where: { userId: user.id },
+            })
+        ).length !== 0;
+
+    if (!solved) {
+        return (
+            <div className="text-white">
+                YOU MUST SOLVE THE PROBLEM TO SEE THE SOLTIONS
+            </div>
+        );
+    }
+
     const isUUID =
         z.string().uuid().safeParse(params.solution_id).success &&
         z.string().uuid().safeParse(params.problem_id).success;
--- src/app/problems/[problem_id]/solutions/page.tsx
+++ src/app/problems/[problem_id]/solutions/page.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 import prisma from "@/core/db/orm";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const AllSolutions = async ({ params }: { params: { problem_id: string } }) => {
+    const session = await getServerSession(authOptions);
+    if (!session) return <>NOT LOGGED IN</>;
+
+    const user = await prisma.user.findUnique({
+        where: { id: session.user.id },
+    });
+    if (!user) {
+        return <>Failed :(</>;
+    }
+
+    const solved =
+        (
+            await prisma.solution.findMany({
+                where: { userId: user.id },
+            })
+        ).length !== 0;
+
+    if (!solved) {
+        return (
+            <div className="text-white">
+                YOU MUST SOLVE THE PROBLEM TO SEE THE SOLTIONS
+            </div>
+        );
+    }
+
     const solutions = await prisma.solution.findMany({
         where: { problemId: params.problem_id },
     });
