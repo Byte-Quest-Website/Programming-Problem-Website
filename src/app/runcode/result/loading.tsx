45d24dd3e9c926ff9ea5f2a2ed79dce6c2a56fab
FusionSid <siddheshadsv@icloud.com>
2023-09-15 13:27:26 +1200

    feat: add loader to each page

--- src/app/runcode/result/loading.tsx
+++ src/app/runcode/result/loading.tsx
@@ -1,30 +1,6 @@
 "use client";
 
-import React from "react";
-import { CSSProperties } from "react";
-import BeatLoader from "react-spinners/BeatLoader";
-
-const override: CSSProperties = {
-    display: "block",
-    margin: "0 auto",
-    borderColor: "red",
-};
-
-function Loading() {
-    return (
-        <div className="h-screen">
-            <BeatLoader
-                className="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                color={"#ffffff"}
-                loading={true}
-                cssOverride={override}
-                size={25}
-                aria-label="Loading Spinner"
-                data-testid="loader"
-            />
-        </div>
-    );
-}
+import Loading from "@/core/components/Loading";
 
 const loading = () => {
     return <Loading />;
---
+++ src/core/components/Loading.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import { CSSProperties } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
+
+const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+};
+
+export default function Loading() {
+    return (
+        <div className="h-screen">
+            <BeatLoader
+                className="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                color={"#ffffff"}
+                loading={true}
+                cssOverride={override}
+                size={25}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </div>
+    );
+}
